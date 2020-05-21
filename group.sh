ebf9eba1cf40f4e5c5dfd979028cdf8d26b5e5ae
Michal Ratajewski <michal.ratajewski@pearson.com>
2020-05-21 15:59:51 +0200

    Adding interesting script created as a part of exercises in Chapter 1 of a DataCamp bash course.

---
+++ group.sh
@@ -0,0 +1,2 @@
+#!/usr/bash
+cat animals.txt | cut -d " " -f 2 | sort | uniq -c
---
+++ replace_names.sh
@@ -0,0 +1,5 @@
+#!/usr.bash
+
+# Creates a sed pipe to a new file
+cat soccer_scores.csv | sed 's/Cherno/Cherno City/g' | sed 's/Arda/Arda United/g' | tee soccer_scores_edited.csv
+
