e2ae09dc6b0c3c230a44229e9ae0491c5d6c0a99
Michal Ratajewski <michal.ratajewski@pearson.com>
2020-05-22 07:09:17 +0200

    Adding scripts that cover basics of arrays in bash.

---
+++ associative_array.sh
@@ -0,0 +1,7 @@
+!#/usr/local/bin/bash
+
+# An associative array has been created for you
+declare -A model_metrics=([model_accuracy]=98 [model_name]="knn" [model_f1]=0.82)
+
+# Print out just the keys
+echo ${!model_metrics[@]}
